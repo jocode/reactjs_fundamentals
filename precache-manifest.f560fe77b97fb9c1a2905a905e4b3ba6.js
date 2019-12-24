2d4ab6343274bb678ac693d16edb7e2c73a83bfc
Johan Mosquera <joncamilomoni@gmail.com>
2019-12-24 14:48:38 -0500

    Updates

---
+++ precache-manifest.f560fe77b97fb9c1a2905a905e4b3ba6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b24cfaa73d16020b1f20f621cf5d998",
+    "url": "/reactjs_fundamentals/index.html"
+  },
+  {
+    "revision": "856b96be6ca845c9dc80",
+    "url": "/reactjs_fundamentals/static/css/2.9720967d.chunk.css"
+  },
+  {
+    "revision": "fb55b6ca8a993362e663",
+    "url": "/reactjs_fundamentals/static/css/main.a04d89f8.chunk.css"
+  },
+  {
+    "revision": "856b96be6ca845c9dc80",
+    "url": "/reactjs_fundamentals/static/js/2.6c37424e.chunk.js"
+  },
+  {
+    "revision": "1f5ef408952bff931b056e1e80c1730c",
+    "url": "/reactjs_fundamentals/static/js/2.6c37424e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb55b6ca8a993362e663",
+    "url": "/reactjs_fundamentals/static/js/main.01c82d12.chunk.js"
+  },
+  {
+    "revision": "2a4647f35ad6c05f0e90",
+    "url": "/reactjs_fundamentals/static/js/runtime-main.b1facaf5.js"
+  },
+  {
+    "revision": "608d335d775c0d4590dd8d9405b006cb",
+    "url": "/reactjs_fundamentals/static/media/astronautas.608d335d.svg"
+  },
+  {
+    "revision": "ed7c58eafe17eff3b2557c0369b20264",
+    "url": "/reactjs_fundamentals/static/media/badge-header.ed7c58ea.svg"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/reactjs_fundamentals/static/media/estrellas.0abcdfcd.svg"
+  },
+  {
+    "revision": "6841730f1bbe3235e7a0cdc7d244d244",
+    "url": "/reactjs_fundamentals/static/media/logo.6841730f.svg"
+  },
+  {
+    "revision": "eb33b95cb710ecf5f24e4eb0bab1eee5",
+    "url": "/reactjs_fundamentals/static/media/platziconf_logo.eb33b95c.svg"
+  }
+]);
